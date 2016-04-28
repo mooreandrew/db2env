3c18a8fe3ff35b680d657bef4d1ee43ea186cd9f
Andrew Moore <mooreandrewlr@gmail.com>
2016-04-28 12:17:19 +0100

    Run commands as user

--- provision.sh
+++ provision.sh
@@ -6,8 +6,10 @@ yum install -y libstdc++.i686 pam.i686
 
 /vagrant/expc/db2_install -b /opt/ibm/db2/V10.5
 
+useradd db2inst1
+
 /opt/ibm/db2/V10.5/instance/db2icrt -p 50000 -u db2inst1 db2inst1
 
 su - db2inst1
-db2start
-sdb2sampl
+sudo runuser -l  db2inst1 -c 'db2start'
+sudo runuser -l  db2inst1 -c 'db2sampl'
