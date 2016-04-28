ce5e8bc9fb01f1dd50dbe47f244507ee5a400fa9
Andrew Moore <mooreandrewlr@gmail.com>
2016-04-28 19:59:32 +0100

    Fixed Script

--- provision.sh
+++ provision.sh
@@ -8,15 +8,18 @@ if [ ! -f /vagrant/v10.5_linuxx64_expc.tar  ]; then
   exit 1
 fi
 
-tar xf /vagrant/v10.5_linuxx64_expc.tar --overwrite
-tar xf /vagrant/IBM-MQ-Light-Linux-x64-developer-1.0.2.0-L151130.2.tar.gz --overwrite
+if [ ! -d /vagrant/expc ]; then
+  tar xf /vagrant/v10.5_linuxx64_expc.tar --overwrite -C /vagrant/
+fi
 
-yum upgrade -y
+if [ ! -d /vagrant/mqlight-developer-1.0.2.0-L151130.2 ]; then
+  tar xf /vagrant/IBM-MQ-Light-Linux-x64-developer-1.0.2.0-L151130.2.tar.gz --overwrite -C /vagrant/
+fi
 setenforce 0
 
-/vagrant/expc/db2prereqcheck
-
-yum install -y libstdc++.i686 pam.i686
+#/vagrant/expc/db2prereqcheck
+yum update libstdc++-4.8.5-4.el7 pam -y
+yum install -y pam.i686 libaio
 
 /vagrant/expc/db2_install -b /opt/ibm/db2/V10.5
 
@@ -25,7 +28,6 @@ echo "db2inst1:db2inst1"| sudo chpasswd
 
 /opt/ibm/db2/V10.5/instance/db2icrt -p 50000 -u db2inst1 db2inst1
 
-su - db2inst1
 sudo runuser -l  db2inst1 -c 'db2start'
 sudo runuser -l  db2inst1 -c 'db2sampl'
 
