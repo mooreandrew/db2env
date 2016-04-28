9e8d3d8063821656a8c9625a4c97d2c7bdee87c1
Andrew Moore <mooreandrewlr@gmail.com>
2016-04-28 11:54:38 +0100

    Fixed db2 install pth

--- provision.sh
+++ provision.sh
@@ -4,10 +4,10 @@ setenforce 0
 
 yum install -y libstdc++.i686 pam.i686
 
-/vagrant/expc/db2_install
+/vagrant/expc/db2_install -b /opt/ibm/db2/V10.5
 
 /opt/ibm/db2/V10.5/instance/db2icrt -p 50000 -u db2inst1 db2inst1
 
 su - db2inst1
 db2start
-db2sampl
+sdb2sampl
