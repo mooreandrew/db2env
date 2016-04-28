185a12dd011a42a9e9c0c274053df93ae11f5998
Andrew Moore <mooreandrewlr@gmail.com>
2016-04-28 11:26:49 +0100

    Example

---
+++ provision.sh
@@ -0,0 +1,13 @@
+yum upgrade -y
+setenforce 0
+/vagrant/expc/db2prereqcheck
+
+yum install -y libstdc++.i686 pam.i686
+
+/vagrant/expc/db2_install
+
+/opt/ibm/db2/V10.5/instance/db2icrt -p 50000 -u db2inst1 db2inst1
+
+su - db2inst1
+db2start
+db2sampl
